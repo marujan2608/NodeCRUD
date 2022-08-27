eaa72eb042855a8c5fff7b95ef2964c25710ee1a
marujan2608 <sn.marujan9508@gmail.com>
2022-08-27 11:32:06 +0530

    handle err 500

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 module.exports = function(app){
 
     // All defined endpoints
-    app.use('/get', (req, res) => {
+    app.use('/sam', (req, res) => {
         res.status(200).send("Hello");
     })
 
     // All undefined endpoints
     app.use((req, res) => {
         res.status(404).json({message : '404 - Not found'});
+    })
+
+    // All unhandled error
+    app.use((err, req, res) => {
+        res.status(500).json({message : '500 - Internal server error', error : err});
     })
 }
